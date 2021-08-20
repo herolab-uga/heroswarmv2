018a7a6fa4c4cbac0aaeb6a5c30f215ce860d347
Michael Starks <starksmichael224@gmail.com>
2021-08-20 14:25:57 -0400

    fixed package setup

--- ROS2/ros_ws/src/robot_controller/setup.py
+++ ROS2/ros_ws/src/robot_controller/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "controller = network_controller.controller:main",
+            "controller = robot_controller.controller:main",
         ],
     },
 )
