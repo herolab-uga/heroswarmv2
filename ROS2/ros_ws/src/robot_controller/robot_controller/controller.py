ea0297e17054e5f3ebaa3a3f821af9a6aa5a08c8
Michael Starks <starksmichael224@gmail.com>
2021-08-22 21:53:37 -0400

    debugging

--- ROS2/ros_ws/src/robot_controller/robot_controller/controller.py
+++ ROS2/ros_ws/src/robot_controller/robot_controller/controller.py
@@ -76,14 +76,14 @@ class Controller(Node):
 
        
 
-        odom_data = []
+        odom_data = [0,0,0,0,0]
 
         # Get odom data from arduino
         for index in range(5):
             bytes = bytearray()
             for i in range(4):
                 bytes.append(data[4*index + i])
-            odom_data.append(struct.unpack('f', bytes)[0])
+            odom_data[index] = struct.unpack('f', bytes)[0]
 
         # Adds Twist data
         odom_msg.twist.twist.linear.x = odom_data[3] * math.cos(odom_data[2])
