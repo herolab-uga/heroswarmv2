61f615b345bc923c46e81f64192fd05ce49af83a
Michael Starks <starksmichael224@gmail.com>
2021-08-25 22:31:04 -0400

    debugging

--- ROS2/ros_ws/src/robot_controller/robot_controller/controller.py
+++ ROS2/ros_ws/src/robot_controller/robot_controller/controller.py
@@ -136,6 +136,7 @@ class Controller(Node):
         if z_angular is self.angular_z_velo:
             return
 
+        print("Here 1")
         # Sends the velocity information to the feather board
         self.send_velocity([x_velo,y_velo,z_angular])
 
