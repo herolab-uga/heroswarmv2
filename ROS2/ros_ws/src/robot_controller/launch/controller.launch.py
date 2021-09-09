475b989481c669e8025eb3945131f15a46586bda
Michael Starks <starksmichael224@gmail.com>
2021-09-08 21:49:43 -0400

    debugging

--- ROS2/ros_ws/src/robot_controller/launch/controller.launch.py
+++ ROS2/ros_ws/src/robot_controller/launch/controller.launch.py
@@ -2,19 +2,19 @@
 
 import os
 import socket
-from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return(
+    return LaunchDescription(
         [
             Node(
                 name=socket.gethostname(),
                 package='robot_controller',
                 executable='controller',
                 namespace=socket.gethostname(),
+
             )
         ]
     )
