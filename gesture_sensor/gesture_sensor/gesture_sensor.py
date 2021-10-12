64214f9cdf58d055b480ba50a03ea55b948c3af8
Michael Starks <starksmichael224@gmail.com>
2021-10-12 15:25:06 -0400

    debug

--- gesture_sensor/gesture_sensor/gesture_sensor.py
+++ gesture_sensor/gesture_sensor/gesture_sensor.py
@@ -1,13 +1,22 @@
 import board
 from adafruit_apds9960.apds9960 import APDS9960
 import digitalio
-
+import busio
+import adafruit_apds9960.apds9960
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_apds9960.apds9960.APDS9960(i2c)
 
 class Gesture():
     def __init__(self):
         i2c = board.I2C()
         apds = APDS9960(i2c)
         apds.enable_gesture = True
+        sensor.enable_gesture = True
+        gesture = sensor.gesture()
+        while gesture == 0:
+            gesture = sensor.gesture()
+        print('Saw gesture: {0}'.format(gesture))
+
         while True:
             self.gesture_controls(apds)
 
