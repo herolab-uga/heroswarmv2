e6180559f41a7ba4a9ced48c08127a773844f2c3
Michael Starks <starksmichael224@gmail.com>
2021-11-13 13:58:33 -0500

    debugging

--- ROS1/ros_ws/src/robot_controller/src/controller.py
+++ ROS1/ros_ws/src/robot_controller/src/controller.py
@@ -125,11 +125,10 @@ class Controller:
                 if self.last_call["time"] == None:
                     continue
                 elif time.time() - self.last_call["time"] > .500:
-                    if not (0 == self.linear_x_velo and 0 == self.linear_y_velo and 0 == self.angular_z_velo):
-                        self.send_velocity([0, 0, 0])
-                        self.linear_x_velo = 0
-                        self.linear_y_velo = 0
-                        self.angular_z_velo = 0
+                    self.send_velocity([0, 0, 0])
+                    self.linear_x_velo = 0
+                    self.linear_y_velo = 0
+                    self.angular_z_velo = 0
 
     def read_imu(self, event=None) -> None:
 
