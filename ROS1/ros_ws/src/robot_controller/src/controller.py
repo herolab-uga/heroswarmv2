c9e9af681f57d3d228981988f45d7ba3ad27957c
Michael Starks <starksmichael224@gmail.com>
2021-11-20 10:18:21 -0500

    removed read lock

--- ROS1/ros_ws/src/robot_controller/src/controller.py
+++ ROS1/ros_ws/src/robot_controller/src/controller.py
@@ -124,11 +124,9 @@ class Controller:
     
     def auto_stop(self):
         while True:
-            with self.velo_lock:
-                lock = self.last_call["time"]
-            if lock == None:
+            if self.last_call["time"] == None:
                 continue
-            elif time.time() - lock > 0.12:
+            elif time.time() - self.last_call["time"] > 0.12:
                 if not (self.linear_x_velo == 0 and self.linear_y_velo == 0 and self.angular_z_velo == 0):
                     self.send_velocity([0, 0, 0])
                     self.linear_x_velo = 0
