58669bd7857700f5af3552e4bdb137c9de03d122
Michael Starks <starksmichael224@gmail.com>
2022-01-24 15:21:46 -0500

    debugging

--- ROS1/ros_ws/src/camera_server/scripts/rendezvous_demo.py
+++ ROS1/ros_ws/src/camera_server/scripts/rendezvous_demo.py
@@ -6,7 +6,7 @@ from rps.utilities.barrier_certificates import *
 from rps.utilities.misc import *
 from rps.utilities.controllers import *
 
-num_robots = 4
+num_robots = 2
 
 wrapper = ServerWrapper(num_robots)
 
@@ -16,6 +16,9 @@ for iteration in range(iterations):
 
     # Get the position of the robots using the camera server
     current_pos = wrapper.get_position_global()
+    current_pos_xy = []
+    current_pos_xy.append([x[:1] for x in current_pos])
+    print(current_pos)
 
     vels = []
     for robot in range(num_robots):
@@ -23,12 +26,13 @@ for iteration in range(iterations):
         y_sum = 0
         for i in range(num_robots):
             x_sum += current_pos[i][0] - current_pos[robot][0]
-            y_sum += current_pos[i][1] - current_pos[robot][0]
+            y_sum += current_pos[i][1] - current_pos[robot][1]
         vels.append([x_sum,y_sum])
     
-    vels = si_barrier_cert(vels,current_pos)
+    # passing current pos remove theta
+    vels = si_barrier_cert(vels,current_pos_xy])
 
-    vels = si_to_uni_dyn(vels,current_pos)
+    vels = si_to_uni_dyn(vels,current_pos_xy)
 
     wrapper.set_velocities(vels)
     wrapper.step()
