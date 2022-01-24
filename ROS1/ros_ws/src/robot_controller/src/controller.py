7c09d74a623a3d23c9ed38091fdbae6ef2d8ed8d
Michael Starks <starksmichael224@gmail.com>
2022-01-24 13:01:29 -0500

    debugging

--- ROS1/ros_ws/src/robot_controller/src/controller.py
+++ ROS1/ros_ws/src/robot_controller/src/controller.py
@@ -319,8 +319,8 @@ class Controller:
         self.bus = smbus.SMBus(1)
 
         # Init the i2c bus
-        self.light_sensor = False
-        self.environment_sensor = False
+        self.light_sensor = True
+        self.environment_sensor = True
         self.imu_sensor = False
         self.proximity_sensor = False
         self.global_pos = False
