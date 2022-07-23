01e4be07aaca4dff23ae3006b2cae99988d2e8e8
Michael Starks <starksmichael224@gmail.com>
2022-07-23 09:52:26 -0400

    seeking demo

--- ROS1/ros_ws/src/camera_server/scripts/seeking_demo.py
+++ ROS1/ros_ws/src/camera_server/scripts/seeking_demo.py
@@ -10,27 +10,40 @@ def main():
     # Collision avoidance
     si_barrier_cert = create_single_integrator_barrier_certificate_with_boundary()
 
-    # Create SI to UNI dynamics tranformation
+    # Create SI to UNI dynamics transformation
     si_to_uni_dyn, uni_to_si_states = create_si_to_uni_mapping()
 
-    num_robots = 5 # Number of robots to use
+    num_robots = 3 # Number of robots to use
 
-    wrapper = ServerWrapper(num_robots) # Swarm warpper object
+    wrapper = ServerWrapper(num_robots) # Swarm wrapper object
 
-    robot_positions = wrapper.get_position_global() # Returns a list of robot positions and orientaions
+    robot_positions = wrapper.get_position_global() # Returns a list of robot positions and orientations
     
     # Returns a list of list containing (rgbw) values
     # robot_light_sensor_data[0][0] returns the red value from the sensor of the first robot
     # robot_light_sensor_data[n][m < 4] returns a value (w,g,b or w ) from the sensor of robot N
-    robot_light_sensor_data = warpper.get_light() 
-    current_pos_xy = [x[:2] for x in current_pos] # Current (x,y) positions of the robots
+    robot_light_sensor_data = wrapper.get_light() 
+    current_pos_xy = [x[:2] for x in robot_positions] # Current (x,y) positions of the robots
 
-    #The control function needs to return a list of velocites for the robots
+    # Get index of max value in list+
+    light_intensity = [x[3] for x in robot_light_sensor_data]
+    light_intensity_max_index = light_intensity.index(max(light_intensity))
+
+    # Get the position of the light source
+    light_intensity_pos = current_pos_xy[light_intensity_max_index]
+
+    # Generate velocities to move towards the light source
+    vels = []
+    for (index,robot) in enumerate(robot_positions):
+        if not index == light_intensity_max_index: 
+            vels.append([light_intensity_pos[0] - robot[0], light_intensity_pos[1] - robot[1]])
+        else:
+            vels.append([robot[0]+1,robot[1]])
     
     # Checks robot velocities for possible collisions
     vels = si_barrier_cert(np.asarray(vels).transpose(),np.asarray(current_pos_xy).transpose())
     
-    vels = si_to_uni_dyn(vels,np.asarray(current_pos).transpose())
+    vels = si_to_uni_dyn(vels,np.asarray(robot_positions).transpose())
     
     wrapper.set_velocities(vels.transpose())
     wrapper.step()
