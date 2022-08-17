c4d951edd9f42aa2eaf1058bc49f42a64e7f836e
Michael Starks <starksmichael224@gmail.com>
2022-08-17 14:37:14 -0400

    removed autostop

--- ROS1/ros_ws/src/robot_controller/src/controller.py
+++ ROS1/ros_ws/src/robot_controller/src/controller.py
@@ -149,7 +149,7 @@ class Controller:
             z_angular = 0
         
         self.stop_timer = threading.Timer(0.5,self.stop)
-        self.stop_timer.start()
+        # self.stop_timer.start()
 
         if not (x_velo == self.linear_x_velo and z_angular == self.angular_z_velo):
             # Logs the data
