d222d8cdf33f98cfa9aa27b8862680c47070a122
Michael Starks <starksmichael224@gmail.com>
2022-09-30 10:04:47 -0400

    multi robot distribution

--- ROS1/ros_ws/src/camera_server/test/distribution_demo.py
+++ ROS1/ros_ws/src/camera_server/test/distribution_demo.py
@@ -2,6 +2,7 @@ import sys
 import signal
 import random
 import math
+import traceback
 from ServerWrapper import *
 import numpy as np
 from utilities.graph import *
@@ -20,65 +21,49 @@ si_barrier_cert = create_single_integrator_barrier_certificate()
 # Create SI to UNI dynamics tranformation
 si_to_uni_dyn, uni_to_si_states = create_si_to_uni_mapping()
 
-
-num_robots = 1
-
-xpos = 200#BOARD_SIZE_X * random.random()
-ypos = 150#BOARD_SIZE_Y * random.random()
-
 rows = 720
 cols = 1080
 scale = 1
 decay = 4
+num_sources = 1
 
-light_source = np.array([xpos, ypos])
-light_source_pixels = np.array([int(xpos*cols/BOARD_SIZE_X),int(ypos*rows/BOARD_SIZE_Y)])
+# Create a list of light sources
+light_sources = []
+light_source_pixels = []
+for i in range(num_sources):
+
+    xpos = random.uniform(0,2.5 * board_scale)
+    ypos = random.uniform(0,1.7 * board_scale)
+    light_sources.append([xpos,ypos])
+    light_source_pixels.append([int(xpos * cols/BOARD_SIZE_X),int(ypos * rows/BOARD_SIZE_Y)])
+
 luminosity = 10 #random.randint(1,10)
 
-print("Source Location: {x},{y}".format(x=light_source[0],y=light_source[1]))
+for source in range(len(light_sources)):
+    print("Source: {source_number} | Source Location: {x},{y}".format(source_number=source, x=light_sources[source][0],y=light_sources[source][1]))
 
-
-# def pop_sensor_old(sensor_array):
-#     for i in np.arange(0,BOARD_SIZE_X,BOARD_SIZE_X/cols):
-#         for j in np.arange(0,BOARD_SIZE_Y,BOARD_SIZE_Y/rows):
-#             distance = (np.sqrt(np.square(i - light_source[0]) + np.square(j - light_source[1])))*scale
-#             if distance < 0.001:
-#                 sensor_array[int((i*rows)/BOARD_SIZE_X)][int((j*cols)/BOARD_SIZE_Y)] = luminosity
-#             else:
-#                 sensor_array[int((i*rows)/BOARD_SIZE_X)][int((j*cols)/BOARD_SIZE_Y)] = luminosity - 20*decay*math.log(distance) #(luminosity/(4 * np.pi * (distance ** 2)))
-#     wrapper.set_intensity(intensity=sensor_array,max_intensity=luminosity)
-#     return sensor_array
-
-# print("BEFORE")
-# for i in range(int((rows/2))-20,int((rows/2))+20):
-#     for j in range(int(cols/2)-20,int(cols/2)+20):
-#         print(sensor_array[j][i],end=" ")
-#     print("\n")
 def NormalizeData(data):
     return (data - np.min(data)) / (np.max(data) - np.min(data))
 
 def pop_sensor():
     sensor_array = np.zeros((cols,rows))
-    for xi in range(0,cols,1):
-        for yi in range(0,rows,1):
-            distance = (np.sqrt(np.square((xi * BOARD_SIZE_X/cols) - light_source[0]) + np.square((yi * BOARD_SIZE_Y/rows) - light_source[1])))*scale
-            if distance < 1:
-                # print("X: {x} Y: {y} Distance: {d}".format(x=xi * BOARD_SIZE_X/cols,y=yi * BOARD_SIZE_Y/rows,d=distance))
-                sensor_array[xi][yi] = luminosity
-            else:
-                #if luminosity - 20*decay*math.log(distance) < 0:
-                #    sensor_array[xi][yi] = 0
-                #else:
-                sensor_array[xi][yi] = luminosity - 20*decay*math.log(distance) #(luminosity/(4 * np.pi * (distance ** 2)))
-                #sensor_array[xi][yi] = luminosity/(4*np.pi*np.square(distance)) #(luminosity/(4 * np.pi * (distance ** 2)))
-    #norm = np.linalg.norm(sensor_array)
-    #print(norm)
+    for source in range(len(light_sources)):
+        for xi in range(0,cols,1):
+            for yi in range(0,rows,1):
+                distance = (np.sqrt(np.square((xi * BOARD_SIZE_X/cols) - light_sources[source][0]) + np.square((yi * BOARD_SIZE_Y/rows) - light_sources[source][1])))*scale
+                if distance < 1:
+                    # print("X: {x} Y: {y} Distance: {d}".format(x=xi * BOARD_SIZE_X/cols,y=yi * BOARD_SIZE_Y/rows,d=distance))
+                    sensor_array[xi][yi] = luminosity
+                else:
+                    sensor_array[xi][yi] = luminosity - 20*decay*math.log(distance) #(luminosity/(4 * np.pi * (distance ** 2)))
     sensor_array = NormalizeData(sensor_array)
     return sensor_array
 
 iterations = 1000
 
 sensor_array = pop_sensor()
+
+num_robots = 3
 
 wrapper = ServerWrapper(num_robots)
 
@@ -106,11 +91,14 @@ wrapper.set_intensity(intensity=sensor_array)
 #         print("{:.2f}".format(sensor_array[i][j]),end=" ")
 #     print("\n")
 
-# while True:
-#     continue
-
 print("Running")
 inverse_scale = 1/board_scale
+c_v = np.zeros((num_robots,2))
+w = np.zeros((num_robots))
+
+# Distance between two points
+def dist(pos1,pos2):
+    return np.sqrt(np.square(pos1[0]-pos2[0]) + np.square(pos1[1]-pos2[1]))
 
 for iteration in range(iterations):
 
@@ -127,21 +115,26 @@ for iteration in range(iterations):
         current_pos_xy = np.asarray([x[:2] for x in current_pos]).transpose()
         # Initialize a velocity vector
         dxi = []
+        c_x = 0
+        c_y = 0
+        w_xy = 0
+        for xi in range(0,cols,10):
+            for yi in range(0,rows,10):
+                sensor_value = sensor_array[xi][yi]
+                distances = np.zeros((num_robots))
+                for robot in range(num_robots):
+                    distances[robot] = dist(current_pos[robot]*board_scale,np.array([xi*BOARD_SIZE_X/cols,yi*BOARD_SIZE_Y/rows]))
+                min_index = np.argmin(distances)
+                c_v[min_index][0] += ((xi * BOARD_SIZE_X/cols) * sensor_value)
+                c_v[min_index][1] += ((yi * BOARD_SIZE_Y/rows) * sensor_value)
+                w[min_index] += sensor_value
+        #w_xy = rows*cols/100
+
         for robot in range(num_robots):
-            c_x = 0
-            c_y = 0
-            w_xy = 0
-            for xi in range(0,cols,5):
-                for yi in range(0,rows,5):
-                    sensor_value = sensor_array[xi][yi]
-                    c_x += ((xi * BOARD_SIZE_X/cols) * sensor_value)
-                    c_y += ((yi * BOARD_SIZE_Y/rows) * sensor_value)
-                    w_xy += sensor_value
-            #w_xy = rows*cols/100
-            c_x = c_x/w_xy * inverse_scale
-            c_y = c_y/w_xy * inverse_scale
+            c_x = c_v[robot][0]/w[robot] * inverse_scale
+            c_y = c_v[robot][1]/w[robot] * inverse_scale
             # print c_x and c_y
-            print("Cx: {} | Cy: {}".format(c_x,c_y))
+            # print("Cx: {} | Cy: {}".format(c_x,c_y))
             # print("Robot current pos: {} ".format(current_pos[robot]))
 
             dxi.append([c_x - current_pos[robot][0], c_y - current_pos[robot][1]])
@@ -158,8 +151,8 @@ for iteration in range(iterations):
         wrapper.set_velocities(dxu.transpose())
         wrapper.step(60)
 
-    except Exception as e:
-        print(e)
+    except Exception:
+        print(traceback.format_exc())
         print("Exiting Code")
         wrapper.stop()
         break
