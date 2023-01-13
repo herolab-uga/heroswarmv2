79c74c06291749dcb53e946d9309a948df51d086
Michael Starks <starksmichael224@gmail.com>
2023-01-13 14:30:49 -0500

    debugging

--- ROS2/ros_ws/src/robot_controller/robot_controller/robot_controller.py
+++ ROS2/ros_ws/src/robot_controller/robot_controller/robot_controller.py
@@ -487,7 +487,7 @@ class Controller(Node):
         self.IMU = None
 
         # Creates subscribers for positions topics
-        if self.get_parameter("global_pos").get_parameter_value().string_value == True:
+        if self.get_parameter("global_pos").get_parameter_value().string_value == "True":
             self.pos_sub_global = self.create_subscription(
                 RobotPos,"/positions", self.get_pos_global,10)
         else:
@@ -531,29 +531,33 @@ class Controller(Node):
         ###_________________Enables Sensor Data Publishers________________###
 
         # Creates a publisher for the magnetometer, bmp and humidity sensor
-        if self.get_parameter("environment") == True or self.get_parameter("all_sensors").get_parameter_value().boolean_value == True:
+        if self.get_parameter("environment").get_parameter_value().string_value == "True" or self.get_parameter("all_sensors").get_parameter_value().string_value == True:
             self.environment_pub = self.create_publisher(
                 Environment, "environment", 10)
             self.environment_timer = self.create_timer(.1,self.pub_environment)
 
         # Creates a publisher for imu data
-        if self.get_parameter("imu") == True or self.get_parameter("all_sensors").get_parameter_value().boolean_value == True:
+        if self.get_parameter("imu").get_parameter_value().string_value == "True" \
+            or self.get_parameter("all_sensors").get_parameter_value().string_value == "True":
             self.imu_pub = self.create_publisher(Imu, "imu/data_raw", 10)
             self.imu_timer = self.create_timer(.1,self.pub_imu) # not working
 
         # Creates a publisher for the light sensor
-        if self.get_parameter("controller/light") == True or self.get_parameter("all_sensors").get_parameter_value().boolean_value == True:
+        if self.get_parameter("light").get_parameter_value().string_value == "True" \
+            or self.get_parameter("all_sensors").get_parameter_value().string_value == "True":
             self.light_pub = self.create_publisher(Light, 'light', 10)
             self.light_timer = self.create_timer(
                 .2, self.pub_light)
 
         # Creates a publisher for a proximity sensor
-        if self.get_parameter("proximity") == True or self.get_parameter("all_sensors").get_parameter_value().boolean_value == True:
+        if self.get_parameter("proximity").get_parameter_value().string_value == "True" \
+            or self.get_parameter("all_sensors").get_parameter_value().string_value == "True":
             self.prox_pub = self.create_publisher(Int16, "proximity",10)
             self.environment_timer = self.create_timer(
                 .2, self.pub_proximity)
 
-        if self.get_parameter("/mic") == True or self.get_parameter("all_sensors").get_parameter_value().boolean_value == True:
+        if self.get_parameter("/mic").get_parameter_value().string_value == "True"\
+             or self.get_parameter("all_sensors").get_parameter_value().string_value == "True":
             self.mic_pub = self.create_publisher(Float32, "mic", 10)
             self.mic_timer = self.create_timer(
                 .2, self.pub_mic)
