d5cd7302c6187ee3e2ac0d929df86ed366aeb130
Michael Starks <starksmichael224@gmail.com>
2023-01-13 16:45:38 -0500

    debugging

--- ROS2/ros_ws/src/robot_controller/robot_controller/robot_controller.py
+++ ROS2/ros_ws/src/robot_controller/robot_controller/robot_controller.py
@@ -333,7 +333,7 @@ class Controller(Node):
             struct.pack('f'*len(values), *values) + bytes("\n".encode())
         # fails to send last byte over I2C, hence this needs to be added
         try:
-            self.get_logger.info("Sending message: {opcode} {data}".format(
+            self.get_logger().info("Sending message: {opcode} {data}".format(
                 opcode=opcode, data=values))
             # Writes the values to the i2c
             self.serial.write(byteList)
