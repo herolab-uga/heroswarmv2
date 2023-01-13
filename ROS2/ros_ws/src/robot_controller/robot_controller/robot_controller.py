b0900c3e573d1049cdd1d541f9ae4551cc043902
Michael Starks <starksmichael224@gmail.com>
2023-01-13 16:51:45 -0500

    debugging

--- ROS2/ros_ws/src/robot_controller/robot_controller/robot_controller.py
+++ ROS2/ros_ws/src/robot_controller/robot_controller/robot_controller.py
@@ -153,14 +153,14 @@ class Controller(Node):
         self.get_logger().info("X Linear: {x} Y Linear: {y} Z Angular: {z}".format(
             x=msg.linear.x, y=msg.linear.y, z=msg.angular.z))
         # Reads ths twist message x linear velocity
-        if abs(msg.linear.x) == 0.001:
+        if abs(msg.linear.x) > 0.001:
             direction_lin = msg.linear.x / abs(msg.linear.x)
             x_velo = np.min(np.max(msg.linear.x, -0.1), 0.1)
         else:
             x_velo = 0
 
         # Reads the twist message z angular velocity
-        if abs(msg.angular.z) == .01:
+        if abs(msg.angular.z) > .01:
 
             z_angular = np.min(np.max(msg.angular.z, .01), 1.85)
         else:
