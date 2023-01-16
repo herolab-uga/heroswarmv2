453f0702c7efab8bd2a0f276517edf9e52731b85
Michael Starks <starksmichael224@gmail.com>
2023-01-16 10:32:12 -0500

    debugging

--- ROS2/ros_ws/src/robot_controller/robot_controller/robot_controller.py
+++ ROS2/ros_ws/src/robot_controller/robot_controller/robot_controller.py
@@ -423,7 +423,7 @@ class Controller(Node):
         self.arduino = 0x08
 
         self.i2c = board.I2C()
-        self.serial = serial.Serial("/dev/serial1", baudrate=115200, timeout=1)
+        self.serial = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=1)
         self.name = self.get_namespace()
 
         # self.manager = mp.Manager()
