e7295c39fd930c63898d50ac2ead01100c8b6941
Michael Starks <starksmichael224@gmail.com>
2023-01-16 12:49:38 -0500

    debugging

--- ROS2/ros_ws/src/robot_controller/robot_controller/robot_controller.py
+++ ROS2/ros_ws/src/robot_controller/robot_controller/robot_controller.py
@@ -361,17 +361,18 @@ class Controller(Node):
 
     def __init__(self):
         super().__init__("robot_controller")
+
+        self.declare_parameter("all_sensors")
+        self.declare_parameter("mic")
+        self.declare_parameter("proximity")
+        self.declare_parameter("environment")
+        self.declare_parameter("light")
+        self.declare_parameter("imu")
+        self.declare_parameter("global_pos")
+
         self.get_logger().info("PARAMETERS KEYS")
         for key in self._parameters.keys():
             self.get_logger().info(str(key) + " " + str(self._parameters[key].value))
-
-        # self.declare_parameter("all_sensors")
-        # self.declare_parameter("mic")
-        # self.declare_parameter("proximity")
-        # self.declare_parameter("environment")
-        # self.declare_parameter("light")
-        # self.declare_parameter("imu")
-        # self.declare_parameter("global_pos")
 
         # Arduino Device Address
         self.arduino = 0x08
