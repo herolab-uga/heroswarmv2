99b31d3eba238bec12aa632fac8b359b64968666
samuel lemus <slems.s760@gmail.com>
2024-12-15 11:54:38 -0500

    updated with script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#sudo apt update && sudo apt upgrade
+#sudo apt install raspi-config python3-pip
+
+#! remove the entry of 'console=serial0,115200' in /boot/firmware/cmdline.txt
+#! comment out the spi configuration in /boot/firmware/config.txt
+#! reboot device
+
+#pip install \
+# adafruit-board-toolkit \
+# adafruit-circuitpython-bmp280 \
+# adafruit-circuitpython-lis3mdl \
+# adafruit-circuitpython-sht31d \
+# adafruit-circuitpython-apds9960 \
+# adafruit-circuitpython-lsm6ds
+
+#! follow setup @ https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debs.html
