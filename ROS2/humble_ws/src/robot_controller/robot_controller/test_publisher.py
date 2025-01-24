ca2c1bf39e91bf92681561ec4f6b4556caf5dce1
Rishab Seshadri <rishab.seshadri@gmail.com>
2025-01-24 14:03:52 -0500

    RS messing with test_pub

--- ROS2/humble_ws/src/robot_controller/robot_controller/test_publisher.py
+++ ROS2/humble_ws/src/robot_controller/robot_controller/test_publisher.py
@@ -1,4 +1,5 @@
 import rclpy
+import time
 
 from rclpy.node import Node 
 from geometry_msgs.msg import Twist 
@@ -8,23 +9,32 @@ class TestPublisher(Node):
     def __init__(self): 
 
         super().__init__('test_publisher') 
-        self.pub = self.create_publisher(Twist, 'swarmduckworth/cmd_vel', 10) 
+        self.pub = self.create_publisher(Twist, 'swarmphoenix/cmd_vel', 10)
+        self.start_time = time.perf_counter() 
         self.start() 
 
-    def start(self): 
+    def start(self):        
 
         twist_msg = Twist() 
-        #twist_msg.linear.x = -1.0
-        twist_msg.angular.x = 1.0
+        #twist_msg.linear.x = -1.0        
         
-        while True: 
-
+        while True:             
+            current_time = time.perf_counter()
+            if current_time > self.start_time + 10:
+                twist_msg.linear.y = 0.0                
+            else:
+                twist_msg.linear.y = 2.0
+                
             self.pub.publish(twist_msg) 
 
 def main(args=None): 
 
     rclpy.init(args=args) 
     publisher = TestPublisher() 
+
+
+    # testing here
+
     rclpy.spin(publisher)
     publisher.destroy_node() 
     rclpy.shutdown() 
