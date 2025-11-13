4b8c88a00e4a7a8423f5fafcdff2edefa264be96
Michael Starks <starksmichael224@gmail.com>
2025-11-12 17:17:38 -0700

    removed extra lib

--- Arduino/swarmv3_firmware/src/pdm/pdm.cpp
+++ Arduino/swarmv3_firmware/src/pdm/pdm.cpp
@@ -6,25 +6,10 @@
 #include <FreeRTOS.h>
 #include <semphr.h>
 
-// number of samples read
-volatile int32_t samples_read = 0;
-
 // buffer to read samples into, each sample is 16-bits
 short gPDMSampleBuffer[256];
 
-static SemaphoreHandle_t gPDMSemaphore;
-
-int get_mic_reading(uint16_t length, void* args)
-{
-    uint32_t mic_reading[1] = {0};
-    LOCK_SEMAPHORE(gPDMSemaphore);
-    mic_reading[0] = samples_read;
-    UNLOCK_SEMAPHORE(gPDMSemaphore);
-
-    uart_send_message(0x3, mic_reading, sizeof(mic_reading));
-}
-
-void onPDMdata()
+static int32_t onPDMdata()
 {
     
     // query the number of bytes available
@@ -34,17 +19,22 @@ void onPDMdata()
     PDM.read(gPDMSampleBuffer, bytes_available);
 
     // 16-bit, 2 bytes per sample
-    LOCK_SEMAPHORE(gPDMSemaphore);
-    samples_read = bytes_available / 2;
-    UNLOCK_SEMAPHORE(gPDMSemaphore);
+    return (bytes_available / 2);
+}
+
+int get_mic_reading(uint16_t length, void* args)
+{
+    uint32_t mic_reading[1] = {0};
+
+    mic_reading[0] = onPDMdata();
+
+    uart_send_message(0x3, mic_reading, sizeof(mic_reading));
+
+    return 0;
 }
 
 void init_pdm()
 {
-    gPDMSemaphore = xSemaphoreCreateMutex();
-
-    PDM.onReceive(onPDMdata);
-
     if (!PDM.begin(1, 16000))
     {
         Serial.println("Failed to start PDM!");
